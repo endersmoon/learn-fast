ea3e2f64d74890835fbc69c64189662b345c09a9
Tushar Debnath <endersmoon@gmail.com>
2026-03-23 17:14:54 +0530

    fixed spacing

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <div className='bg-muted min-h-screen  sm:p-12'>
+      <div className='sm:max-w-90 mx-auto bg-white min-h-screen sm:min-h-211 '>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </div>
+    </div>
   </StrictMode>,
-)
+);
